9e3a820633e03669bb7570905db62735b750a7c0
Han <todayunityengineer@gmail.com>
2017-01-29 23:30:35 +0900

    Add Temp Resources

---
+++ Assets/Scripts/Common/Sound/Sound.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Sound 
+{
+	public static void PlayBGM(int id)
+	{
+
+	}
+
+	public static void PlaySE(int id)
+	{
+
+	}
+}
---
+++ Assets/Scripts/Data/API/API.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class API 
+{
+	public static void GetAPI(string url)
+	{
+
+	}
+}
